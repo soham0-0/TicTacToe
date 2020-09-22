70fba9418437314a8ce56ef9e66b280f965bf985
Soham Pal <sohampal101@gmail.com>
2020-09-23 00:45:43 +0530

    added necessary files

---
+++ Makefile
@@ -0,0 +1,7 @@
+OBJS = main.cpp gameLogic.cpp
+CC = g++
+COMPILER_FLAGS = -w
+LINKER_FLAGS = -lSDL2
+OBJ_NAME = TicTacToe.out
+all : $(OBJS)
+	$(CC) $(OBJS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
---
+++ gameLogic.h
@@ -0,0 +1,8 @@
+#ifndef GAMELOGIC_H
+#define GAMELOGIC_H
+#include<vector>
+using namespace std;
+int evaluate(vector <vector<char>> board);
+bool isNext(vector <vector<char>> board);
+vector<int> findNext(vector <vector<char>> board, bool isMaxer);
+#endif 
